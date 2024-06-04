4b1c3aac4f93c7d6bebafaa95e79e712eccec9cf
Connor Jensen <connorjjensen@churchofjesuschrist.org>
2024-06-04 12:04:10 -0600

    add metadata and cache loadBlogPost

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -5,12 +5,19 @@ import BlogHero from '@/components/BlogHero';
 import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import { BLOG_TITLE } from '@/constants';
+
+export async function generateMetadata({params}) {
+  const { frontmatter } = await loadBlogPost(params.postSlug)
+  return {
+    title: `${frontmatter.title} • ${BLOG_TITLE}`,
+    description: frontmatter.abstract
+  }
+}
 
 async function BlogPost({params}) {
 
   const { frontmatter, content } = await loadBlogPost(params.postSlug)
-
-  console.log(frontmatter, content)
 
   return (
     <article className={styles.wrapper}>
